ac2165d93ec3612516ced56c00567c47dc9e5ad7
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2023-11-30 12:21:02 -0300

    .

--- Verilog/Loteria.v
+++ Verilog/Loteria.v
@@ -7,23 +7,13 @@ module Jogo (
     input reset, // Reset
 
     // Saídas
-    output [3:0] num0, // Número 0
-    output [3:0] num1, // Número 1
-    output [3:0] num2, // Número 2
-    output [3:0] num3, // Número 3
-    output [3:0] num4, // Número 4
-
-    output win, // Venceu?
-    output [1:0] p0, // Prêmio
-
-    // FPGA
-    output HEX0 [6:0], // Número 4
-    output HEX1 [6:0], // Número 3
-    output HEX2 [6:0], // Número 2
-    output HEX3 [6:0], // Número 1
-    output HEX4 [6:0], // Número 0
-    output HEX5 [6:0], // Prêmio
-    output LED8 // Venceu?
+    output [6:0] HEX0, // Número 4
+    output [6:0] HEX1, // Número 3
+    output [6:0] HEX2, // Número 2
+    output [6:0] HEX3, // Número 1
+    output [6:0] HEX4, // Número 0
+    output [6:0] HEX5, // Prêmio
+    output LEDG8 // Venceu?
 );
 
 // Números da loteria - 50967
@@ -46,6 +36,14 @@ parameter s7 = 4'b0111; // Prêmio 0
 parameter s8 = 4'b1000; // Prêmio 1
 parameter s9 = 4'b1001; // Prêmio 2
 
+reg [3:0] num0; // Número 0
+reg [3:0] num1; // Número 1
+reg [3:0] num2; // Número 2
+reg [3:0] num3; // Número 3
+reg [3:0] num4; // Número 4
+reg win; // Venceu?
+reg [1:0] p0; // Prêmio
+
 initial begin
     win <= 0;
     p0 <= 4'b00;
@@ -54,13 +52,14 @@ initial begin
     num2 <= 4'b0000;
     num3 <= 4'b0000;
     num4 <= 4'b0000;
-    HEX0 <= 7'b0111111;
-    HEX1 <= 7'b0111111;
-    HEX2 <= 7'b0111111;
-    HEX3 <= 7'b0111111;
-    HEX4 <= 7'b0111111;
-    HEX5 <= 7'b0111111;
-    LED8 <= 0;
+    HEX0 <= 7'b1000000;
+    HEX1 <= 7'b1000000;
+    HEX2 <= 7'b1000000;
+    HEX3 <= 7'b1000000;
+    HEX4 <= 7'b1000000;
+    HEX5 <= 7'b1000000;
+    LEDG8 <= 0;
+    state <= s0;
 end
 
 always @(posedge clk) begin
@@ -72,13 +71,13 @@ always @(posedge clk) begin
         num2 <= 4'b0000;
         num3 <= 4'b0000;
         num4 <= 4'b0000;
-        HEX0 <= 7'b0111111;
-        HEX1 <= 7'b0111111;
-        HEX2 <= 7'b0111111;
-        HEX3 <= 7'b0111111;
-        HEX4 <= 7'b0111111;
-        HEX5 <= 7'b0111111;
-        LED8 <= 0;
+        HEX0 <= 7'b1000000;
+        HEX1 <= 7'b1000000;
+        HEX2 <= 7'b1000000;
+        HEX3 <= 7'b1000000;
+        HEX4 <= 7'b1000000;
+        HEX5 <= 7'b1000000;
+        LEDG8 <= 0;
     end
     else begin
         case (state)
@@ -161,62 +160,61 @@ always @(posedge clk) begin
             end
         endcase
     end
-
+end
     // Primeiro número inserido
-    HEX0[6] <= (~num0[0] & ~num0[1] & ~num0[3]) + (~num0[0] & num0[2]) | (~num0[0] & num0[1] & num0[3]) | (num0[0] & ~num0[1] & ~num0[2]);
-    HEX0[5] <= (~num0[0] & ~num0[1]) | (~num0[0] & ~num0[2] & ~num0[3]) | (~num0[1] & ~num0[2]) | (~num0[0] & num0[2] & num0[3]);
-    HEX0[4] <= (~num0[1] & ~num0[2]) | (~num0[0] & num0[3]) | (~num0[0] &  num0[1]);
-    HEX0[3] <= (~num0[0] & ~num0[1] & ~num0[3]) | (~num0[0] & ~num0[1] & num0[2]) | (~num0[0] & num0[2] &  ~num0[3]) | (~num0[0] &  num0[1] &  ~num0[2] & num0[3]) | (num0[0] &  ~num0[1] & ~num0[2]);
-    HEX0[2] <= (~num0[1] & ~num0[2] & ~num0[3]) | (~num0[0] & num0[2] & ~num0[3]);
-    HEX0[1] <= (~num0[0] & ~num0[2] & ~num0[3]) | (~num0[0] & num0[1] & ~num0[2]) | (~num0[0] & num0[1] & ~num0[3]) | (num0[0] & ~num0[1] & ~num0[2]);
-    HEX0[0] <= (~num0[0] & ~num0[1] & num0[2]) | (~num0[0] & num0[1] & ~num0[2]) | (~num0[0] & num0[1] & ~num0[3]) | (num0[0] & ~num0[1] & ~num0[2]);
+    assign HEX4[6] = ~((~num0[0] & ~num0[1] & ~num0[3]) + (~num0[0] & num0[2]) | (~num0[0] & num0[1] & num0[3]) | (num0[0] & ~num0[1] & ~num0[2]));
+    assign HEX4[5] = ~((~num0[0] & ~num0[1]) | (~num0[0] & ~num0[2] & ~num0[3]) | (~num0[1] & ~num0[2]) | (~num0[0] & num0[2] & num0[3]));
+    assign HEX4[4] = ~((~num0[1] & ~num0[2]) | (~num0[0] & num0[3]) | (~num0[0] &  num0[1]));
+    assign HEX4[3] = ~((~num0[0] & ~num0[1] & ~num0[3]) | (~num0[0] & ~num0[1] & num0[2]) | (~num0[0] & num0[2] &  ~num0[3]) | (~num0[0] &  num0[1] &  ~num0[2] & num0[3]) | (num0[0] &  ~num0[1] & ~num0[2]));
+    assign HEX4[2] = ~((~num0[1] & ~num0[2] & ~num0[3]) | (~num0[0] & num0[2] & ~num0[3]));
+    assign HEX4[1] = ~((~num0[0] & ~num0[2] & ~num0[3]) | (~num0[0] & num0[1] & ~num0[2]) | (~num0[0] & num0[1] & ~num0[3]) | (num0[0] & ~num0[1] & ~num0[2]));
+    assign HEX4[0] = ~((~num0[0] & ~num0[1] & num0[2]) | (~num0[0] & num0[1] & ~num0[2]) | (~num0[0] & num0[1] & ~num0[3]) | (num0[0] & ~num0[1] & ~num0[2]));
 
     // Segundo número inserido
-    HEX1[6] <= (~num1[0] & ~num1[1] & ~num1[3]) | (~num1[0] & num1[2]) | (~num1[0] & num1[1] & num1[3]) | (num1[0] & ~num1[1] & ~num1[2]);
-    HEX1[5] <= (~num1[0] & ~num1[1]) | (~num1[0] & ~num1[2] & ~num1[3]) | (~num1[1] & ~num1[2]) | (~num1[0] & num1[2] & num1[3]);
-    HEX1[4] <= (~num1[1] & ~num1[2]) | (~num1[0] & num1[3]) | (~num1[0] &  num1[1]);
-    HEX1[3] <= (~num1[0] & ~num1[1] & ~num1[3]) | (~num1[0] & ~num1[1] & num1[2]) | (~num1[0] & num1[2] &  ~num1[3]) | (~num1[0] &  num1[1] &  ~num1[2] & num1[3]) | (num1[0] &  ~num1[1] & ~num1[2]);
-    HEX1[2] <= (~num1[1] & ~num1[2] & ~num1[3]) | (~num1[0] & num1[2] & ~num1[3]);
-    HEX1[1] <= (~num1[0] & ~num1[2] & ~num1[3]) | (~num1[0] & num1[1] & ~num1[2]) | (~num1[0] & num1[1] & ~num1[3]) | (num1[0] & ~num1[1] & ~num1[2]);
-    HEX1[0] <= (~num1[0] & ~num1[1] & num1[2]) | (~num1[0] & num1[1] & ~num1[2]) | (~num1[0] & num1[1] & ~num1[3]) | (num1[0] & ~num1[1] & ~num1[2]);
+    assign HEX3[6] = ~((~num1[0] & ~num1[1] & ~num1[3]) | (~num1[0] & num1[2]) | (~num1[0] & num1[1] & num1[3]) | (num1[0] & ~num1[1] & ~num1[2]));
+    assign HEX3[5] = ~((~num1[0] & ~num1[1]) | (~num1[0] & ~num1[2] & ~num1[3]) | (~num1[1] & ~num1[2]) | (~num1[0] & num1[2] & num1[3]));
+    assign HEX3[4] = ~((~num1[1] & ~num1[2]) | (~num1[0] & num1[3]) | (~num1[0] &  num1[1]));
+    assign HEX3[3] = ~((~num1[0] & ~num1[1] & ~num1[3]) | (~num1[0] & ~num1[1] & num1[2]) | (~num1[0] & num1[2] &  ~num1[3]) | (~num1[0] &  num1[1] &  ~num1[2] & num1[3]) | (num1[0] &  ~num1[1] & ~num1[2]));
+    assign HEX3[2] = ~((~num1[1] & ~num1[2] & ~num1[3]) | (~num1[0] & num1[2] & ~num1[3]));
+    assign HEX3[1] = ~((~num1[0] & ~num1[2] & ~num1[3]) | (~num1[0] & num1[1] & ~num1[2]) | (~num1[0] & num1[1] & ~num1[3]) | (num1[0] & ~num1[1] & ~num1[2]));
+    assign HEX3[0] = ~((~num1[0] & ~num1[1] & num1[2]) | (~num1[0] & num1[1] & ~num1[2]) | (~num1[0] & num1[1] & ~num1[3]) | (num1[0] & ~num1[1] & ~num1[2]));
 
     // Terceiro número inserido
-    HEX2[6] <= (~num2[0] & ~num2[1] & ~num2[3]) | (~num2[0] & num2[2]) | (~num2[0] & num2[1] & num2[3]) | (num2[0] & ~num2[1] & ~num2[2]);
-    HEX2[5] <= (~num2[0] & ~num2[1]) | (~num2[0] & ~num2[2] & ~num2[3]) | (~num2[1] & ~num2[2]) | (~num2[0] & num2[2] & num2[3]);
-    HEX2[4] <= (~num2[1] & ~num2[2]) | (~num2[0] & num2[3]) | (~num2[0] &  num2[1]);
-    HEX2[3] <= (~num2[0] & ~num2[1] & ~num2[3]) | (~num2[0] & ~num2[1] & num2[2]) | (~num2[0] & num2[2] &  ~num2[3]) | (~num2[0] &  num2[1] &  ~num2[2] & num2[3]) | (num2[0] &  ~num2[1] & ~num2[2]);
-    HEX2[2] <= (~num2[1] & ~num2[2] & ~num2[3]) | (~num2[0] & num2[2] & ~num2[3]);
-    HEX2[1] <= (~num2[0] & ~num2[2] & ~num2[3]) | (~num2[0] & num2[1] & ~num2[2]) | (~num2[0] & num2[1] & ~num2[3]) | (num2[0] & ~num2[1] & ~num2[2]);
-    HEX2[0] <= (~num2[0] & ~num2[1] & num2[2]) | (~num2[0] & num2[1] & ~num2[2]) | (~num2[0] & num2[1] & ~num2[3]) | (num2[0] & ~num2[1] & ~num2[2]);
+    assign HEX2[6] = ~((~num2[0] & ~num2[1] & ~num2[3]) | (~num2[0] & num2[2]) | (~num2[0] & num2[1] & num2[3]) | (num2[0] & ~num2[1] & ~num2[2]));
+    assign HEX2[5] = ~((~num2[0] & ~num2[1]) | (~num2[0] & ~num2[2] & ~num2[3]) | (~num2[1] & ~num2[2]) | (~num2[0] & num2[2] & num2[3]));
+    assign HEX2[4] = ~((~num2[1] & ~num2[2]) | (~num2[0] & num2[3]) | (~num2[0] &  num2[1]));
+    assign HEX2[3] = ~((~num2[0] & ~num2[1] & ~num2[3]) | (~num2[0] & ~num2[1] & num2[2]) | (~num2[0] & num2[2] &  ~num2[3]) | (~num2[0] &  num2[1] &  ~num2[2] & num2[3]) | (num2[0] &  ~num2[1] & ~num2[2]));
+    assign HEX2[2] = ~((~num2[1] & ~num2[2] & ~num2[3]) | (~num2[0] & num2[2] & ~num2[3]));
+    assign HEX2[1] = ~((~num2[0] & ~num2[2] & ~num2[3]) | (~num2[0] & num2[1] & ~num2[2]) | (~num2[0] & num2[1] & ~num2[3]) | (num2[0] & ~num2[1] & ~num2[2]));
+    assign HEX2[0] = ~((~num2[0] & ~num2[1] & num2[2]) | (~num2[0] & num2[1] & ~num2[2]) | (~num2[0] & num2[1] & ~num2[3]) | (num2[0] & ~num2[1] & ~num2[2]));
 
     // Quarto número inserido
-    HEX3[6] <= (~num3[0] & ~num3[1] & ~num3[3]) | (~num3[0] & num3[2]) | (~num3[0] & num3[1] & num3[3]) | (num3[0] & ~num3[1] & ~num3[2]);
-    HEX3[5] <= (~num3[0] & ~num3[1]) | (~num3[0] & ~num3[2] & ~num3[3]) | (~num3[1] & ~num3[2]) | (~num3[0] & num3[2] & num3[3]);
-    HEX3[4] <= (~num3[1] & ~num3[2]) | (~num3[0] & num3[3]) | (~num3[0] &  num3[1]);
-    HEX3[3] <= (~num3[0] & ~num3[1] & ~num3[3]) | (~num3[0] & ~num3[1] & num3[2]) | (~num3[0] & num3[2] &  ~num3[3]) | (~num3[0] &  num3[1] &  ~num3[2] & num3[3]) | (num3[0] &  ~num3[1] & ~num3[2]);
-    HEX3[2] <= (~num3[1] & ~num3[2] & ~num3[3]) | (~num3[0] & num3[2] & ~num3[3]);
-    HEX3[1] <= (~num3[0] & ~num3[2] & ~num3[3]) | (~num3[0] & num3[1] & ~num3[2]) | (~num3[0] & num3[1] & ~num3[3]) | (num3[0] & ~num3[1] & ~num3[2]);
-    HEX3[0] <= (~num3[0] & ~num3[1] & num3[2]) | (~num3[0] & num3[1] & ~num3[2]) | (~num3[0] & num3[1] & ~num3[3]) | (num3[0] & ~num3[1] & ~num3[2]);
+    assign HEX1[6] = ~((~num3[0] & ~num3[1] & ~num3[3]) | (~num3[0] & num3[2]) | (~num3[0] & num3[1] & num3[3]) | (num3[0] & ~num3[1] & ~num3[2]));
+    assign HEX1[5] = ~((~num3[0] & ~num3[1]) | (~num3[0] & ~num3[2] & ~num3[3]) | (~num3[1] & ~num3[2]) | (~num3[0] & num3[2] & num3[3]));
+    assign HEX1[4] = ~((~num3[1] & ~num3[2]) | (~num3[0] & num3[3]) | (~num3[0] &  num3[1]));
+    assign HEX1[3] = ~((~num3[0] & ~num3[1] & ~num3[3]) | (~num3[0] & ~num3[1] & num3[2]) | (~num3[0] & num3[2] &  ~num3[3]) | (~num3[0] &  num3[1] &  ~num3[2] & num3[3]) | (num3[0] &  ~num3[1] & ~num3[2]));
+    assign HEX1[2] = ~((~num3[1] & ~num3[2] & ~num3[3]) | (~num3[0] & num3[2] & ~num3[3]));
+    assign HEX1[1] = ~((~num3[0] & ~num3[2] & ~num3[3]) | (~num3[0] & num3[1] & ~num3[2]) | (~num3[0] & num3[1] & ~num3[3]) | (num3[0] & ~num3[1] & ~num3[2]));
+    assign HEX1[0] = ~((~num3[0] & ~num3[1] & num3[2]) | (~num3[0] & num3[1] & ~num3[2]) | (~num3[0] & num3[1] & ~num3[3]) | (num3[0] & ~num3[1] & ~num3[2]));
 
     // Quinto número inserido
-    HEX4[6] <= (~num4[0] & ~num4[1] & ~num4[3]) | (~num4[0] & num4[2]) | (~num4[0] & num4[1] & num4[3]) | (num4[0] & ~num4[1] & ~num4[2]);
-    HEX4[5] <= (~num4[0] & ~num4[1]) | (~num4[0] & ~num4[2] & ~num4[3]) | (~num4[1] & ~num4[2]) | (~num4[0] & num4[2] & num4[3]);
-    HEX4[4] <= (~num4[1] & ~num4[2]) | (~num4[0] & num4[3]) | (~num4[0] &  num4[1]);
-    HEX4[3] <= (~num4[0] & ~num4[1] & ~num4[3]) | (~num4[0] & ~num4[1] & num4[2]) | (~num4[0] & num4[2] &  ~num4[3]) | (~num4[0] &  num4[1] &  ~num4[2] & num4[3]) | (num4[0] &  ~num4[1] & ~num4[2]);
-    HEX4[2] <= (~num4[1] & ~num4[2] & ~num4[3]) | (~num4[0] & num4[2] & ~num4[3]);
-    HEX4[1] <= (~num4[0] & ~num4[2] & ~num4[3]) | (~num4[0] & num4[1] & ~num4[2]) | (~num4[0] & num4[1] & ~num4[3]) | (num4[0] & ~num4[1] & ~num4[2]);
-    HEX4[0] <= (~num4[0] & ~num4[1] & num4[2]) | (~num4[0] & num4[1] & ~num4[2]) | (~num4[0] & num4[1] & ~num4[3]) | (num4[0] & ~num4[1] & ~num4[2]);
+    assign HEX0[6] = ~((~num4[0] & ~num4[1] & ~num4[3]) | (~num4[0] & num4[2]) | (~num4[0] & num4[1] & num4[3]) | (num4[0] & ~num4[1] & ~num4[2]));
+    assign HEX0[5] = ~((~num4[0] & ~num4[1]) | (~num4[0] & ~num4[2] & ~num4[3]) | (~num4[1] & ~num4[2]) | (~num4[0] & num4[2] & num4[3]));
+    assign HEX0[4] = ~((~num4[1] & ~num4[2]) | (~num4[0] & num4[3]) | (~num4[0] &  num4[1]));
+    assign HEX0[3] = ~((~num4[0] & ~num4[1] & ~num4[3]) | (~num4[0] & ~num4[1] & num4[2]) | (~num4[0] & num4[2] &  ~num4[3]) | (~num4[0] &  num4[1] &  ~num4[2] & num4[3]) | (num4[0] &  ~num4[1] & ~num4[2]));
+    assign HEX0[2] = ~((~num4[1] & ~num4[2] & ~num4[3]) | (~num4[0] & num4[2] & ~num4[3]));
+    assign HEX0[1] = ~((~num4[0] & ~num4[2] & ~num4[3]) | (~num4[0] & num4[1] & ~num4[2]) | (~num4[0] & num4[1] & ~num4[3]) | (num4[0] & ~num4[1] & ~num4[2]));
+    assign HEX0[0] = ~((~num4[0] & ~num4[1] & num4[2]) | (~num4[0] & num4[1] & ~num4[2]) | (~num4[0] & num4[1] & ~num4[3]) | (num4[0] & ~num4[1] & ~num4[2]));
 
     // Prêmio
-    HEX5[6] <= ~p0[1];
-    HEX5[5] <= ~p0[0] & ~p0[1];
-    HEX5[4] <= ~p0[0];
-    HEX5[3] <= ~p0[0] & p0[1] | p0[0] & ~p0[1];
-    HEX5[2] <= ~p0[0] | ~p0[1];
-    HEX5[1] <= ~p0[0] | ~p0[1];
-    HEX5[0] <= ~p0[0];
+    assign HEX5[6] = ~(~p0[1]);
+    assign HEX5[5] = ~(~p0[0] & ~p0[1]);
+    assign HEX5[4] = ~(~p0[0]);
+    assign HEX5[3] = ~((~p0[0] & p0[1]) | (p0[0] & ~p0[1]));
+    assign HEX5[2] = ~(~p0[0] | ~p0[1]);
+    assign HEX5[1] = ~(~p0[0] | ~p0[1]);
+    assign HEX5[0] = ~(~p0[0]);
 
     // Venceu?
-    LED8 <= win;
-end
+    assign LEDG8 = win;
 endmodule
